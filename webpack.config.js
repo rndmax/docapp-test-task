96cf2445112d873a018066df02374c68876720e4
rndmax <korevsky@gmail.com>
2019-11-30 10:32:07 +0300

    Made changes to webpack config, added sass file
    - created a sass file in which inserted bootstrap styles
    - added style-loader, css-loader, postcss-loader, sass-loader to the webpack config, also added a path to search for modules in the data folder - this is for quickly loading json data files, so as not to write file paths

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,41 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'data'), 'node_modules']
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
